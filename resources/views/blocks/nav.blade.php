712231501aead416c1a3d1350891866b90f601c7
AlfaRomeo <galtsov-denis-sevsu@mail.ru>
2023-06-16 17:21:47 +0300

    ~~~~

--- resources/views/blocks/nav.blade.php
+++ resources/views/blocks/nav.blade.php
@@ -1,8 +1,39 @@
-<ul>
-    <li>Item 1</li>
-    <li>Item 2</li>
-    <li>Item 3</li>
-    <li>Item 4</li>
-    <li>Item 5</li>
-    <li>Item 6</li>
-</ul>
+<nav class="nav">
+    <div class="nav__container">
+        <ul class="nav__list">
+            <li class="nav__item">
+                <a class="nav__link" href="{{ url('/home') }}"">Главная</a>
+            </li>
+            <li class="nav__item">
+                <a class="nav__link" href="{{ url('/news') }}"">Новости и акции</a>
+            </li>
+            <li class="nav__item">
+                <a class="nav__link" href="#">Товары</a>
+            </li>
+            <li class="nav__item">
+                <a class="nav__link" href="#">Контакты</a>
+            </li>
+            <li class="nav__item">
+                <a class="nav__link" href="#">Корзина</a>
+            </li>
+            <li class="nav__item">
+                <a class="nav__link" href="#">Инструменты</a>
+            </li>
+            <li class="nav__item">
+                <a class="nav__link" href="#">Аксессуары</a>
+            </li>
+            <li class="nav__item">
+                <a class="nav__link" href="#">Обратная связь</a>
+            </li>
+            <li class="nav__item">
+                <a class="nav__link" href="#">Поддержка</a>
+            </li>
+            <li class="nav__item">
+                <a class="nav__link" href="#">Местоположение</a>
+            </li>
+            <li class="nav__item">
+                <a class="nav__link" href="#">Карта сайта</a>
+            </li>
+        </ul>
+    </div>
+</nav>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <!-- Кодировка страницы -->
     <meta charset="UTF-8">
     <!-- Заголовок страницы в браузере -->
-    <title>Home</title>
+    <title>Главная</title>
     <!-- Адаптив -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <!-- Иконка сайта во вкладке браузера -->
@@ -13,13 +13,19 @@
     <link rel="stylesheet" href="./css/style.css">
     <!-- Подключение скриптов -->
     <script src="./js/script.js" defer></script>
+    <meta>
 </head>
 <body>
+    @include("blocks.nav")
     <div class="page-wrapper">
 
-        @include("blocks.nav")
 
-        Home page
+        <div class="container">
+            <h1>Главная</h1>
+            <div class="content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis possimus tenetur doloribus tempore excepturi necessitatibus quaerat, ea dolor molestiae cumque magni itaque dolores praesentium, totam, vitae sit omnis esse beatae impedit quidem quia quae officiis maxime enim? Nesciunt praesentium nostrum, officia illum quae vitae ratione sunt vel perferendis id, neque omnis similique consequuntur eos architecto veritatis voluptatem assumenda ipsa officiis veniam? Cumque magnam tempore, rerum, ratione perferendis ex voluptatibus doloremque quia repudiandae, veniam qui consequatur. Dignissimos, quia asperiores! Perspiciatis cum magnam nesciunt eius minima quasi excepturi sequi vero consequatur atque, exercitationem, ducimus officia accusamus dolor quae iusto alias repudiandae sit facere sunt consequuntur neque praesentium ipsa! Rerum odio temporibus voluptas accusamus sit, cum minima tenetur corporis beatae sint! Porro provident rerum, sit repudiandae nisi incidunt? Excepturi aliquid ab incidunt repellendus tenetur accusamus molestiae inventore quasi rem dignissimos. Ullam error, magnam in quidem porro veritatis totam animi distinctio incidunt commodi autem maxime eaque assumenda laborum deserunt facilis quis maiores consectetur illum. Necessitatibus reprehenderit, commodi dolor asperiores possimus alias quibusdam obcaecati voluptatem odit repellat fuga. Quaerat, velit saepe harum magnam adipisci animi, nostrum voluptatem aliquid dicta est quis optio. Architecto aliquid voluptatum nisi natus, ipsum sunt eveniet ad. Molestiae neque odit deserunt iusto nostrum consequatur ea vel, reiciendis illum. Ipsum odio culpa ad obcaecati eveniet, perferendis ratione recusandae distinctio nesciunt, dicta dignissimos dolorem commodi qui. Harum officiis perspiciatis porro, aliquid qui rem repellendus, at dignissimos totam itaque quasi quibusdam magni, suscipit laborum a doloremque tempore neque? Unde reprehenderit animi adipisci voluptas, nostrum quos repudiandae, ipsam accusamus consectetur aliquid soluta doloribus, sunt tempore? Nam libero voluptate esse fugiat illo debitis qui deserunt tempore soluta aspernatur sed atque labore mollitia, nesciunt obcaecati perspiciatis asperiores error? Incidunt repudiandae totam itaque quia soluta porro quis repellat consequatur? Optio delectus vel commodi suscipit dolor eum dolorem ut!
+            </div>
+        </div>
 
 
         
