e4360959c48857999887fecc6fd19e9d0f9c0206
Akindin <88840107+Akindin@users.noreply.github.com>
2023-06-16 20:39:37 +0300

    Язык меняется ещё и в теге head

--- public/js/change_language.js
+++ public/js/change_language.js
@@ -1,12 +1,9 @@
 function changeLanguage(language) {
-    let translatable_elements = document.querySelectorAll("[data-text]");
+    let translatable_elements = Array.from(document.querySelectorAll("[data-text]")).concat(Array.from(document.head.querySelectorAll("[data-text]")));
     for (let element of translatable_elements) {
         element.textContent = strings[element.dataset.text][language];
-    }
+    }   
 }
-
-
-
 
 
 
