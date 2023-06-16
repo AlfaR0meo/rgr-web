3148bede237f1cbd94e2def8a0c5e8f551ae206d
AlfaRomeo <galtsov-denis-sevsu@mail.ru>
2023-06-17 02:52:58 +0300

    324324

--- resources/views/admin/news.blade.php
+++ resources/views/admin/news.blade.php
@@ -12,6 +12,18 @@
             <div class="container">
                 <h1>Новости админа</h1>
 
+                <form method="post" class="form" action="{{route('registration.send')}}">
+                    @csrf
+                    <label class="input__title" for="fullname">Имя:</label>
+                    <input class="form__input" type="text" name="fullname" placeholder="Введите ваше имя">
+                    <label class="input__title" for="email">Email:</label>
+                    <input class="form__input" type="text" name="email" placeholder="Введите ваш e-mail">
+                    <label class="input__title" for="login">Логин</label>
+                    <input class="form__input" type="text" name="login" placeholder="Придумайте логин" onblur="checkLoginOccupation()">
+                    <label class="input__title" for="password">Пароль</label>
+                    <input class="form__input" type="text" name="password" placeholder="Придумайте пароль">
+                    <input class="form__input input__title input__submit" type="submit" value="отправить">
+                </form>
             </div>
         </section>
 
