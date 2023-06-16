4d8f4939325ed790788843c9353a6a563b7a1427
AlfaRomeo <galtsov-denis-sevsu@mail.ru>
2023-06-17 00:37:36 +0300

    login admin blade

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,23 +1,15 @@
 <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="ru">
 <head>
-    <!-- Кодировка страницы -->
-    <meta charset="UTF-8">
-    <!-- Заголовок страницы в браузере -->
-    <title>Login admin</title>
-    <!-- Адаптив -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <!-- Иконка сайта во вкладке браузера -->
-    <link rel="icon" href="./favicon.svg" type="image/svg+xml">
-    <!-- Подключение стилей -->
-    <link rel="stylesheet" href="./css/style.css">
-    <!-- Подключение скриптов -->
-    <script src="./js/script.js" defer></script>
+    @include('blocks.head', ['page_title' => 'accessories'])
 </head>
 <body>
     <div class="page-wrapper">
+        
+        @include("blocks.admin-nav")
 
-        <h1>Login admin page</h1>
+        <h1>В гостях у админа на главной</h1>
 
 
         
---
+++ resources/views/blocks/admin-nav.blade.php
@@ -0,0 +1,14 @@
+<header class="page-header">
+    <nav class="nav">
+        <div class="container">
+            <ul class="nav__list">
+                <li class="nav__item">
+                    <a class="nav__link" href="{{ url('/home') }}">Хочу быть пользователем</a>
+                </li>
+                <li class="nav__item">
+                    <button data-lang="ru" data-text="lang" type="button"></button>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
