f11baf98074f7157453e44c4f8ea01df11920c10
AlfaRomeo <galtsov-denis-sevsu@mail.ru>
2023-06-17 02:26:32 +0300

    ftp

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -16,6 +16,11 @@
                     <div class="news__item">2</div>
                     <div class="news__item">3</div>
                     <div class="news__item">4</div>
+                    <div class="news__item">5</div>
+                    <div class="news__item">6</div>
+                    <div class="news__item">7</div>
+                    <div class="news__item">8</div>
+                    <div class="news__item">9</div>
                 </div>
             </div>
         </section>
