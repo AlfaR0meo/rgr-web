f5ca1fa9628519bb8511a326fa042845d0c90d9a
AlfaRomeo <galtsov-denis-sevsu@mail.ru>
2023-06-16 14:57:23 +0300

    front changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <!-- Адаптив -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <!-- Иконка сайта во вкладке браузера -->
-    <!-- <link rel="icon" href="./img/favicon.svg" type="image/svg+xml"> -->
+    <link rel="icon" href="./favicon.svg" type="image/svg+xml">
     <!-- Подключение стилей -->
     <link rel="stylesheet" href="./css/style.css">
     <!-- Подключение скриптов -->
@@ -17,7 +17,7 @@
 <body>
     <div class="page-wrapper">
 
-        
+        Home page
 
 
         
