6dbf3dfa6d271dfb2d273b9892623a8268cad04c
AlfaRomeo <galtsov-denis-sevsu@mail.ru>
2023-06-16 22:41:13 +0300

    products blade

---
+++ resources/views/products.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    @include('blocks.head', ['page_title' => 'Товары'])
+</head>
+<body>
+    <div class="page-wrapper">
+        
+        @include("blocks.nav")
+
+        <h1>Товары</h1>
+
+
+        
+    </div>
+</body>
+</html>
