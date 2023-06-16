dc7f13ce94eccc620e2f1cc3db4ba20a18f222c6
AlfaRomeo <galtsov-denis-sevsu@mail.ru>
2023-06-17 02:01:37 +0300

    ~

---
+++ resources/views/admin/products.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    @include('blocks.head', ['page_title' => 'accessories'])
+</head>
+<body>
+    <div class="page-wrapper">
+        
+        @include("blocks.admin-nav")
+
+        <h1>Товары админа</h1>
+
+
+        
+    </div>
+</body>
+</html>
