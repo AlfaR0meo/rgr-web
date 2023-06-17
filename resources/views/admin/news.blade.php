5e609c10c6a12860f9aff778047d72c01aef5c03
Akindin <88840107+Akindin@users.noreply.github.com>
2023-06-17 06:00:06 +0300

    Tiny MCE подключен к новостям и акциям

--- resources/views/admin/news.blade.php
+++ resources/views/admin/news.blade.php
@@ -2,8 +2,23 @@
 <html lang="ru">
 <head>
     @include('blocks.head', ['page_title' => 'accessories'])
+    <script src="https://cdn.tiny.cloud/1/99vzagw34guk5b2v26t6eyqlbkoqeztrz6zavydzprtsb8oi/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 </head>
 <body>
+    <script defer>
+        tinymce.init({
+            selector: 'textarea',
+            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+            tinycomments_mode: 'embedded',
+            tinycomments_author: 'Author name',
+            mergetags_list: [
+                { value: 'First.Name', title: 'First Name' },
+                { value: 'Email', title: 'Email' },
+            ]
+        });
+    </script>
+
     <div class="page-wrapper s-p">
         
         @include("blocks.admin-nav")
@@ -15,7 +30,7 @@
                 <form method="post" class="form" action="{{route('news.send')}}" enctype="multipart/form-data">
                     @csrf
                     <input type="text" name="title" data-validity_text="skipped_title" required>
-                    <input type="text" name="text" data-validity_text="skipped_text" required>
+                    <textarea type="text" name="text" data-validity_text="skipped_text"></textarea>
                     <input type="file" name="image" data-validity_text="skipped_image" required>
                     <input type="submit" value="отправить">
                 </form>
@@ -28,7 +43,7 @@
                         <div class="news__item">
                             <h2>{{$news_card->title}}</h2>
                             <img src="{{$news_card->image_path}}" alt="">
-                            <p>{{$news_card->text}}</p>
+                            <p>{!!$news_card->text!!}</p>
                             <div class="date">{{$news_card->created_at}}</div>
                         </div>
                     @endforeach
--- resources/views/admin/special.blade.php
+++ resources/views/admin/special.blade.php
@@ -2,8 +2,22 @@
 <html lang="ru">
 <head>
     @include('blocks.head', ['page_title' => 'accessories'])
+    <script src="https://cdn.tiny.cloud/1/99vzagw34guk5b2v26t6eyqlbkoqeztrz6zavydzprtsb8oi/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 </head>
 <body>
+    <script defer>
+        tinymce.init({
+            selector: 'textarea',
+            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+            tinycomments_mode: 'embedded',
+            tinycomments_author: 'Author name',
+            mergetags_list: [
+                { value: 'First.Name', title: 'First Name' },
+                { value: 'Email', title: 'Email' },
+            ]
+        });
+    </script>
     <div class="page-wrapper">
         
         @include("blocks.admin-nav")
@@ -15,7 +29,7 @@
                 <form method="post" class="form" action="{{route('special_offer.send')}}" enctype="multipart/form-data">
                     @csrf
                     <input type="text" name="title" data-validity_text="skipped_title" required>
-                    <input type="text" name="text" data-validity_text="skipped_text" required>
+                    <textarea type="text" name="text" data-validity_text="skipped_text"></textarea>
                     <input type="file" name="image" data-validity_text="skipped_image" required>
                     <input type="submit" value="отправить">
                 </form>
@@ -28,7 +42,7 @@
                             <div class="special_offers__item">
                                 <h2>{{$special_offer->title}}</h2>
                                 <img src="{{$special_offer->image_path}}" alt="">
-                                <p>{{$special_offer->text}}</p>
+                                <p>{!!$special_offer->text!!}</p>
                                 <div class="date">{{$special_offer->created_at}}</div>
                             </div>
                     @endforeach
