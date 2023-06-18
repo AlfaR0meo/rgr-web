e6d6ea4a278fd863b22529bd4cf6c67ab8f589c6
AlfaRomeo <galtsov-denis-sevsu@mail.ru>
2023-06-18 23:02:19 +0300

    ~~~

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -11,7 +11,19 @@
         <section class="section cart">
             <div class="container">
                 <h1 data-text="cart"></h1>
-                
+                <div class="cart__row">
+                    <div class="cart__item">
+                        <h3 class="cart__name">Название товара</h3>
+                        <img class="cart__image" src="./img/home/777.jpg" alt="">
+                        <div class="cart__price">2000 ₽</div>
+                        <div class="cart__count">
+                            <button class="cart__btn cart__btn--dec" type="button">-</button>
+                            <div class="cart__count-number">1</div>
+                            <button class="cart__btn cart__btn--inc" type="button">+</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="cart__total-price">Итого: <span id="total-price">100000</span> ₽</div>
             </div>
         </section>
 
