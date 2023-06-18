8155c2ee1994dd7ad1ddf7678717ec0a98690eb5
AlfaRomeo <galtsov-denis-sevsu@mail.ru>
2023-06-18 19:43:14 +0300

    styles for products

--- resources/views/blocks/head.blade.php
+++ resources/views/blocks/head.blade.php
@@ -2,6 +2,6 @@
 <title data-text="{{ $page_title }}"></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 <link rel="icon" href="./favicon2.svg" type="image/svg+xml">
-<link rel="stylesheet" href="./css/main.css">
+<link rel="stylesheet" href="./css/style.css">
 <script src="./js/script.js" defer></script>
 <script src="./js/change_language.js" defer></script>
