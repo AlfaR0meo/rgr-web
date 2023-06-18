2e468b6b36cc8c06881bbe19fed72c7a0fd82439
AlfaRomeo <galtsov-denis-sevsu@mail.ru>
2023-06-18 19:26:42 +0300

    Шаблон товаров

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -10,7 +10,23 @@
 
         <section class="section products">
             <div class="container">
+
                 <h1 data-text="products"></h1>
+
+                <div class="products__row">
+                    <div class="products__item item-products">
+                        <h3 class="item-products__name">Название товара</h3>
+                        <p class="item-products__text">Описание товара Описание товара Описание товара Описание товара</p>
+                        <img class="item-products__image" src="./img/home/222.jpg" alt="">
+                        <div class="item-products__price">1000₽</div>
+                    </div>
+                    <div class="products__item item-products">
+                        <h3 class="item-products__name">Название товара</h3>
+                        <p class="item-products__text">Описание товара Описание товара Описание товара Описание товара</p>
+                        <img class="item-products__image" src="./img/home/555.jpg" alt="">
+                        <div class="item-products__price">1000₽</div>
+                    </div>
+                </div>
             </div>
         </section>
 
