ce07901741cfcb40082b88af2d846618356f01f7
AlfaRomeo <galtsov-denis-sevsu@mail.ru>
2023-06-19 09:18:19 +0300

    cart form

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -16,7 +16,7 @@
                 <h1 data-text="cart"></h1>
 
                 <div class="cart__row">
-                    <div class="cart__item">
+                    <!-- <div class="cart__item">
                         <h3 class="cart__name">Виолончель</h3>
                         <img class="cart__image" src="./img/home/777.jpg" alt="">
                         <div class="cart__price">2000 ₽</div>
@@ -25,7 +25,7 @@
                             <div class="cart__count-number">1</div>
                             <button class="cart__btn cart__btn--inc" type="button">+</button>
                         </div>
-                    </div>
+                    </div> -->
                     @foreach ($products as $product)
                         <div class="cart__item" data-product_id="{{$product->id}}">
                             <h3 class="cart__name">{{$product->name}}</h3>
@@ -40,16 +40,45 @@
                     @endforeach
                 </div>
 
-                <div class="cart__total-price"><span data-text="total">Итого: </span><span id="total-price"></span> ₽</div>
+                <div class="cart__total-price"><span data-text="total">Итого: </span> <span id="total-price"></span> ₽</div>
             
                 <form action="" class="cart__form">
-                    <input type="hidden" name="product_ids">
+
+                    <div class="cart__form-group">
+                        <input name="product_ids" type="hidden">
+                    </div>
+
+                    <div class="cart__form-group">
+                        <label for="delivery_method" data-text="delivery_method"></label>
+                        <select name="delivery_method" id="delivery_method">
+                            <option value="pickup" data-text="pickup"></option>
+                            <option value="courier" data-text="courier"></option>
+                            <option value="mail" data-text="mail"></option>
+                        </select>
+                    </div>
+
+                    <div class="cart__form-group">
+                        <label for="payment_method" data-text="payment_method"></label>
+                        <select name="payment_method" id="payment_method">
+                            <option value="cash" data-text="">Нал</option>
+                            <option value="cashless" data-text="">Безнал</option>
+                        </select>
+                    </div>
+
                 </form>
+
+
+
+
+
             </div>
         </section>
 
 
         
+
+
+
     </div>
 </body>
 </html>
